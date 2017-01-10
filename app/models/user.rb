3b0f3d936cdc817fbbe5bb5d59f786f1f29a276b
subinhuang <806519191@qq.com>
2017-01-10 11:23:11 +0800

    use the Database judgment the 'admin' condicition

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def admin?
-    email == '2@.com'
+    is_admin
   end
 end

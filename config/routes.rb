89dcbb0c25a9f2afe50a6154ef5d627f6af828fb
subinhuang <806519191@qq.com>
2017-01-11 16:36:57 +0800

    change 'is_hidden' into a button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
-    
+
   devise_for :users
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

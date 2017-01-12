5f22e178d3ca53ff9e4c56496a73f1d960cac353
subinhuang <806519191@qq.com>
2017-01-12 09:26:23 +0800

    hang Resume with User/Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,11 +1,12 @@
 class Job < ApplicationRecord
   scope :published, -> {where(is_hidden: false)}
   scope :recent, -> { order('created_at DESC')}
-  
+  has_many :resume 
   def publish!
     self.is_hidden = false
     self.save
   end
+
   def hide!
     self.is_hidden = true
     self.save

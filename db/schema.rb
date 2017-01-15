ee892e1aad15cd556267026c0d7ebaa67e10d225
subinhuang <806519191@qq.com>
2017-01-15 10:31:04 +0800

    admin can see resume list

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170114103813) do
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
-    t.boolean  "is_admin",         default: true
     t.boolean  "is_hidden",        default: true
   end
 
